152b6b18cf1937c4b1e46f148831fe34e8ca680e
Rahim <shaolinseed@gmail.com>
2022-10-19 20:01:20 +0100

    init error dictionary

---
+++ src/firebase/errors.ts
@@ -0,0 +1,8 @@
+const firebaseErrors = {
+  "Firebase: Error (auth/user-not-found).": "Invalid email or password",
+  "Firebase: Error (auth/wrong-password).": "Invalid email or password",
+  "Firebase: Error (auth/email-already-in-use).":
+    "Email has already been registered",
+}
+
+export { firebaseErrors }
