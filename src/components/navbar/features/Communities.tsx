3cc78a253219f341af38a6d6dd3985526ea60c52
Rahim <shaolinseed@gmail.com>
2022-10-21 17:51:03 +0100

    add create community button

---
+++ src/components/navbar/features/Communities.tsx
@@ -0,0 +1,27 @@
+import { Flex, Icon, MenuItem } from "@chakra-ui/react"
+import React, { useState } from "react"
+import CreateCommunityModal from "../../modal/community/CreateCommunityModal"
+import { BsPlusCircle } from "react-icons/bs"
+
+type CommunitiesProps = {}
+
+const Communities: React.FC<CommunitiesProps> = () => {
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <CreateCommunityModal open={open} closeModal={() => setOpen(false)} />
+      <MenuItem
+        width="100%"
+        fontSize="10pt"
+        _hover={{ bg: "gray.50" }}
+        onClick={() => setOpen(true)}
+      >
+        <Flex>
+          <Icon as={BsPlusCircle} fontSize="20" mr="2" />
+          Create community
+        </Flex>
+      </MenuItem>
+    </>
+  )
+}
+export default Communities
