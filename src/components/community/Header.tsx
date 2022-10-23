37cb1ca2c2ebb425ca5a0220fa23ab523ceb972e
Rahim <shaolinseed@gmail.com>
2022-10-23 10:21:31 +0100

    init community header

---
+++ src/components/community/Header.tsx
@@ -0,0 +1,59 @@
+import { Box, Button, Flex, Icon, Image, Text } from "@chakra-ui/react"
+import React from "react"
+import { FaReddit } from "react-icons/fa"
+import { Community, communityAtom } from "../../store/communityState"
+import useCommunityAtom from "../../hooks/useCommunityAtom"
+
+type Props = {
+  communityData: Community
+}
+
+const Header: React.FC<Props> = ({ communityData }) => {
+  const { community, onJoinOrLeaveCommunity, loading } = useCommunityAtom()
+  // check if user is member of current community
+  const isMember = Boolean(
+    community.find((element) => element.communityId === communityData.id)
+  )
+  return (
+    <Flex direction="column" width="100%" height="146px">
+      <Box height="40%" bg="brand.300" />
+      <Flex justify="center" bg="white" flexGrow={1}>
+        <Flex width="95%" maxWidth="960px">
+          {communityData.imageUrl ? (
+            <Image />
+          ) : (
+            <Icon
+              as={FaReddit}
+              fontSize={64}
+              position="relative"
+              top="-3"
+              color="brand.100"
+              border="4px solid white"
+              borderRadius="50%"
+            />
+          )}
+          <Flex py="10pt" px="16px">
+            <Flex direction="column" mr="6">
+              <Text fontWeight="800" fontSize="18pt">
+                {communityData.id}
+              </Text>
+              <Text fontWeight="600" fontSize="10pt" color="#4fe605">
+                r/{communityData.id}
+              </Text>
+            </Flex>
+            <Button
+              variant={isMember ? "outline" : "solid"}
+              height="30px"
+              px={6}
+              onClick={() => onJoinOrLeaveCommunity(communityData, isMember)}
+              isLoading={loading}
+            >
+              {isMember ? " Joined" : "Join"}
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
+export default Header
