e03afe80b72a15f7dd46a089ae20002ca3c5d55b
Rahim <shaolinseed@gmail.com>
2022-10-23 15:47:58 +0100

    connect form to firestore

--- src/components/posts/NewPostFrm.tsx
+++ src/components/posts/NewPostFrm.tsx
@@ -1,12 +1,26 @@
-import { Flex, Icon } from "@chakra-ui/react"
-import React, { useState } from "react"
+import { Alert, Flex, Icon } from "@chakra-ui/react"
+import React, { ReactComponentElement, useId, useState } from "react"
 import { IoDocumentText, IoImageOutline } from "react-icons/io5"
 import PostTypeTab from "./PostTypeTab"
 import { Text } from "@chakra-ui/react"
 import TextPost from "./postForm/TextPost"
 import ImageVideoPost from "./postForm/ImageVideoPost"
+import { Post } from "../../store/postState"
+import { User } from "firebase/auth"
+import { useRouter } from "next/router"
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore"
+import { firestoreInstance, storage } from "../../firebase/clientApp"
+import { getDownloadURL, ref, uploadString } from "firebase/storage"
 
-type Props = {}
+type Props = {
+  user: User
+}
 
 const postTypeTabs: PostTypeTab[] = [
   {
@@ -29,18 +43,78 @@ export type PostContent = {
   body: string
 }
 
-const NewPostFrm: React.FC<Props> = () => {
+const NewPostFrm: React.FC<Props> = ({ user }) => {
+  const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
   const [openTab, setOpenTab] = useState(postTypeTabs[0].title)
-  const [selectedFile, setSelectedFile] = useState<string>()
+  const [uploadedFile, setUploadedFile] = useState<string>()
   const [textInputs, setTextInputs] = useState<PostContent>({
     title: "",
     body: "",
   })
 
-  const onSelectImage = () => {}
+  const router = useRouter()
+  // Extract community name from router
+  const { communityName } = router.query
 
-  const onCreatePost = async () => {}
+  const onUploadFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileReader = new FileReader()
+
+    if (event.target.files?.[0]) {
+      fileReader.readAsDataURL(event.target.files[0])
+    }
+
+    fileReader.onload = (readerEvent) => {
+      if (readerEvent.target?.result) {
+        setUploadedFile(readerEvent.target.result as string)
+      }
+    }
+  }
+
+  const processCreatePost = async () => {
+    // create new post object
+    const createdPost: Post = {
+      communityId: communityName as string,
+      creatorId: user?.uid,
+      creatorUsername: user.email!.split("@")[0],
+      title: textInputs.title,
+      body: textInputs.body,
+      commentCount: 0,
+      voteStatus: 0,
+      timeCreated: serverTimestamp() as Timestamp,
+    }
+
+    setLoading(true)
+
+    // surround async code in trycatch
+    try {
+      // add doc to firebase (add generates random ID whereas setDoc doesn't)
+      const postDocRef = await addDoc(
+        collection(firestoreInstance, "posts"),
+        createdPost
+      )
+
+      // if post contains an image
+      if (uploadedFile) {
+        // store in firebase storage
+        const imageRef = ref(storage, `posts/${postDocRef.id}/image`)
+        await uploadString(imageRef, uploadedFile, "data_url")
+        const downloadUrl = await getDownloadURL(imageRef)
+
+        await updateDoc(postDocRef, {
+          imageUrl: downloadUrl,
+        })
+      }
+    } catch (error: any) {
+      setError(true)
+    }
+    setLoading(false)
+
+    // router.back()
+
+    // store post in database
+    // store image in firebase storage if necessary
+  }
 
   const onTextChange = async (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -72,14 +146,22 @@ const NewPostFrm: React.FC<Props> = () => {
             <TextPost
               loading={loading}
               textInputs={textInputs}
-              onCreatePost={onCreatePost}
+              processCreatePost={processCreatePost}
               onChange={onTextChange}
             />
           )}
 
-          {openTab === "Images and Video" && <ImageVideoPost />}
+          {openTab === "Images and Video" && (
+            <ImageVideoPost
+              uploadedFile={uploadedFile}
+              onUploadFile={onUploadFile}
+              setUploadedFile={setUploadedFile}
+              setOpenTab={setOpenTab}
+            />
+          )}
         </Flex>
       </Flex>
+      {error && <Alert></Alert>}
     </Flex>
   )
 }
