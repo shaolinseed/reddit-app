9dcaebac2fa665e7a25e49721a83610a92aec651
Rahim <shaolinseed@gmail.com>
2022-10-23 15:49:17 +0100

    pass user state to NewPostFrm

--- src/pages/r/[communityName]/submit.tsx
+++ src/pages/r/[communityName]/submit.tsx
@@ -1,22 +1,32 @@
 import { Box } from "@chakra-ui/react"
 import React from "react"
+import { useAuthState } from "react-firebase-hooks/auth"
 import PageContent from "../../../components/layouts/PageContent"
 import NewPostFrm from "../../../components/posts/NewPostFrm"
+import { auth } from "../../../firebase/clientApp"
 
 type Props = {}
 
-const submit: React.FC<Props> = () => {
+const SubmitPostPage: React.FC<Props> = () => {
+  const [user] = useAuthState(auth)
+
   return (
     <PageContent>
       <>
-        <Box py="14px" px="0px" borderBottom="1px solid" borderColor="white">
+        <Box
+          py="14px"
+          px="0px"
+          borderBottom="1px solid"
+          borderColor="white"
+          fontWeight={700}
+        >
           Create a Post
         </Box>
-        <NewPostFrm />
+        {user && <NewPostFrm user={user} />}
       </>
 
       <></>
     </PageContent>
   )
 }
-export default submit
+export default SubmitPostPage
