d30b873fca9c6572331ddc7813dd963e18b59fc0
Rahim <shaolinseed@gmail.com>
2022-10-24 16:30:32 +0100

    fix community image styling

--- src/components/community/Header.tsx
+++ src/components/community/Header.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Flex, Icon, Image, Text } from "@chakra-ui/react"
-import React from "react"
+import React, { useEffect } from "react"
 import { FaReddit } from "react-icons/fa"
 import { Community, communityAtom } from "../../store/communityState"
 import useCommunityAtom from "../../hooks/useCommunityAtom"
@@ -10,17 +10,34 @@ type Props = {
 
 const Header: React.FC<Props> = ({ communityData }) => {
   const { community, onJoinOrLeaveCommunity, loading } = useCommunityAtom()
+  console.log("------------------")
+
+  console.log(community)
+
+  console.log("------------------")
+
   // check if user is member of current community
   const isMember = Boolean(
-    community.find((element) => element.communityId === communityData.id)
+    community.userSnippets.find(
+      (element) => element.communityId === communityData.id
+    )
   )
+
   return (
     <Flex direction="column" width="100%" height="146px">
       <Box height="40%" bg="brand.300" />
       <Flex justify="center" bg="white" flexGrow={1}>
         <Flex width="95%" maxWidth="960px">
-          {communityData.imageUrl ? (
-            <Image />
+          {community.currentCommunity?.imageUrl ? (
+            <Image
+              borderRadius="full"
+              boxSize="66px"
+              src={community.currentCommunity.imageUrl}
+              alt="Community Image"
+              position="relative"
+              top="-3"
+              border="4px solid white"
+            />
           ) : (
             <Icon
               as={FaReddit}
