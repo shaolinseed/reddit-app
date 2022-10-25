079b7ab1b21fcb4b8fed0a724024759714254d07
Rahim <shaolinseed@gmail.com>
2022-10-25 14:59:53 +0100

    init UserMenu

--- src/components/navbar/features/Features.tsx
+++ src/components/navbar/features/Features.tsx
@@ -1,12 +1,23 @@
-import { Flex, Icon, Menu, MenuButton, MenuList, Text } from "@chakra-ui/react"
+import {
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuList,
+  Text,
+  Image,
+} from "@chakra-ui/react"
 import { useAtom } from "jotai"
 import React from "react"
 import { IoChevronDown } from "react-icons/io5"
 import { authModalAtom } from "../../../store/authModalState"
 import { TiHome } from "react-icons/ti"
 import Communities from "./Communities"
+import useFeatures from "../../../hooks/useFeatures"
 
 const UserMenu: React.FC = () => {
+  const { communityMenu, toggleMenuOpen } = useFeatures()
+
   const [, setAuthModalState] = useAtom(authModalAtom)
   return (
     <Menu>
@@ -17,6 +28,7 @@ const UserMenu: React.FC = () => {
         py="0px"
         px="6px"
         _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+        onClick={toggleMenuOpen}
       >
         <Flex
           align="center"
@@ -24,15 +36,26 @@ const UserMenu: React.FC = () => {
           width={{ base: "auto", lg: "200px" }}
         >
           <Flex align="center">
-            <Icon
-              as={TiHome}
-              color="brand.100"
-              fontSize={24}
-              mr={{ base: 1, md: 2 }}
-            />
+            {communityMenu.selectedMenuElement.imageUrl ? (
+              <Image
+                borderRadius="full"
+                boxSize="24px"
+                mr="2"
+                src={communityMenu.selectedMenuElement.imageUrl}
+                alt="Community Image"
+              />
+            ) : (
+              <Icon
+                as={communityMenu.selectedMenuElement.icon}
+                fontSize={24}
+                mr={{ base: 1, md: 2 }}
+                color={communityMenu.selectedMenuElement.iconColor}
+              />
+            )}
+
             <Flex display={{ base: "none", lg: "flex" }}>
               <Text fontWeight={600} fontSize="11pt" color="brand.100">
-                Home
+                {communityMenu.selectedMenuElement.communityName}
               </Text>
             </Flex>
           </Flex>
